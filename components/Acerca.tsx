d5db0674435bcb37e51f5015a91bea19912cc112
robertopeiro12 <robertopeiro12@gmail.com>
2025-01-22 15:24:01 -0700

    Modified component acerca

--- components/Acerca.tsx
+++ components/Acerca.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+
 const Acerca = () => {
   return (
-    <section className='lg:max-w-6xl mx-auto max-w-lg px-5 lg:px-0'   id="acerca">
-        <p className='lg:text-5xl text-4xl lg:text-start text-center font-bold lg:mb-16 mb-9'> Acerca de la plataforma</p>
-        <div className='flex lg:flex-row flex-col items-center justify-center max-w-6xl'>
-            <p className='lg:text-3xl text-xl lg:text-start  text-center lg:mr-32 mb-10'>
-                La plataforma de BuscoCredito esta hecha
-                con el fin de que las personas puedan encontrar
-                las diferentes opciones de prestamos que los 
-                bancos les pueden ofrecer, simplemente ingresando
-                el tipo de prestamo y la cantidad que requiere. 
-            </p>
+    <section className="w-full bg-white py-16" id="acerca">
+      <div className="lg:max-w-6xl mx-auto max-w-lg px-5 lg:px-0">
+        <h2 className="text-4xl lg:text-5xl font-bold mb-12 text-[#3B82F6]">
+          Acerca de la plataforma
+        </h2>
+        <div className="flex lg:flex-row flex-col items-start justify-between">
+          <p className="text-[#374151] text-xl lg:text-3xl lg:text-start text-center lg:max-w-3xl leading-relaxed">
+            La plataforma de BuscoCredito está hecha con el fin de que las
+            personas puedan encontrar las diferentes opciones de préstamos que
+            los bancos les pueden ofrecer, simplemente ingresando el tipo de
+            préstamo y la cantidad que requiere.
+          </p>
+          <div className="lg:w-[300px] w-full mt-8 lg:mt-0">
             <Image
-            width={1024}
-            height={640}
-            src="/img/prestamo.avif"
-            alt="Acerca"
-            className='lg:w-[50%] w-96'
+              width={300}
+              height={200}
+              src="/img/prestamo.avif"
+              alt="Acerca"
+              className="w-full h-auto rounded-lg"
             />
-
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Acerca
+export default Acerca;
