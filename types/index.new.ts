74a6a2165033eb70c0e71418b09b9b83c16376fe
robertopeiro12 <robertopeiro12@gmail.com>
2025-07-29 12:07:52 -0700

    feat: remove deprecated API test files and HTML page
    - Deleted `public/test-apis.html` as it is no longer needed.
    - Removed `security-test-simple.js`, `test-admin-sdk.js`, `test-firestore-rules.js`, `test-security.js` as they are outdated and not in use.
    - Cleaned up `types/index.new.ts` by removing unused exports.

--- types/index.new.ts
+++
@@ -1,5 +0,0 @@
-// Re-exports organizados por dominio
-export * from './common/ui.types';
-export * from './entities/user.types';
-export * from './entities/admin.types';
-export * from './entities/legacy.types';
