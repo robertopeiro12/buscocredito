2cdb65ac571194435ac6bc716897327f8d1d7e29
GerardoFJ <A00838645@tec.mx>
2026-05-05 12:05:42 -0600

    Fix leak and packages

--- app/firebase.ts
+++ app/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQgSUaavEZJ1wbpQuPBpuPWjynViK1k0g",
-  authDomain: "buscocredito-b3f6d.firebaseapp.com",
-  projectId: "buscocredito-b3f6d",
-  storageBucket: "buscocredito-b3f6d.appspot.com",
-  messagingSenderId: "242507626777",
-  appId: "1:242507626777:web:e6145b1f4884a68f8e2d88",
-  measurementId: "G-HJR923HRPS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase
