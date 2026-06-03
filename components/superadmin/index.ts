55d2cd2bcda2dfe7fd00b36d4a2d8f92c7b1d9f8
robertopeiro12 <robertopeiro12@gmail.com>
2026-06-02 22:20:09 -0600

    chore: remove SuperAdminHeader component, keep SuperAdminLoadingScreen

--- components/superadmin/index.ts
+++ components/superadmin/index.ts
@@ -1,6 +1,6 @@
 // Super Admin Components
 export { SuperAdminSidebar } from "./SuperAdminSidebar";
-export { SuperAdminHeader, SuperAdminLoadingScreen } from "./SuperAdminHeader";
+export { SuperAdminLoadingScreen } from "./SuperAdminHeader";
 export { StatsCards } from "./StatsCards";
 export { AccountsTable } from "./AccountsTable";
 export { AccountDetailModal } from "./AccountDetailModal";
